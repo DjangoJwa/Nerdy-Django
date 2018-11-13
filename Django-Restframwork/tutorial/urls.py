fd814e97ae777fe7ea9baae2571abe22b5b81941
RISMME <python@istruly.sexy>
2018-11-13 18:44:33 +0900

    FIX: urls

--- Django-Restframwork/tutorial/urls.py
+++ Django-Restframwork/tutorial/urls.py
@@ -23,5 +23,5 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framwork.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
