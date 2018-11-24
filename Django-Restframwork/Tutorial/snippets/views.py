136b3c2a82b0543a056527f5a4547f7bd73de0e6
Nerd-Bear <python@istruly.sexy>
2018-11-24 19:28:10 +0900

    UPDATE: user model

--- Django-Restframwork/Tutorial/snippets/views.py
+++ Django-Restframwork/Tutorial/snippets/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 
+from django.contrib.auth.models import User
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -9,8 +10,21 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
